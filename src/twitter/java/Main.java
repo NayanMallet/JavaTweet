325b0d6c3196bb79e8070262a5eccdccf5a6ff11
NayanMallet <nayan.mallet@ynov.com>
2023-05-04 16:06:02 +0200

    [ADD] Tweet body

--- src/twitter/java/Main.java
+++ src/twitter/java/Main.java
@@ -1,7 +1,13 @@
 package twitter.java;
 
+import twitter.java.exceptions.ExceptionsReader;
+import twitter.java.tweet.Tweet;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ExceptionsReader {
+        Tweet myTweet = new Tweet("ehfggfefgnfds");
+        Tweet myPrivateTweet = new Tweet("zizi", true);
+        myPrivateTweet.show();
+        myTweet.show();
     }
 }
--- src/twitter/java/tweet/Tweet.java
+++ src/twitter/java/tweet/Tweet.java
@@ -1,14 +1,43 @@
 package twitter.java.tweet;
 
+import twitter.java.exceptions.ExceptionsReader;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Tweet {
-    private String m_message;
+    private static String m_message;
     private int m_likes = 0;
     private int m_retweets = 0;
     private int m_signets = 0;
-    private boolean isPrivate = false;
+    private boolean m_isPrivate = false;
+    private static String m_creationDate;
 
-    public Tweet(String message) {
+    public Tweet(String message) throws ExceptionsReader {
+        if (message.isEmpty()) {
+            throw new ExceptionsReader("You can't post an empty tweet !");
+        } else if (message.length() > 280) {
+            throw new ExceptionsReader("Your tweet must not exceed 280 characters !");
+        }
         m_message = message;
+        LocalDateTime myDateObj = LocalDateTime.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("h:mm a · d MMM yyyy", new Locale("fr"));
+        m_creationDate = myDateObj.format(myFormatObj);
     }
+
+    public Tweet(String message, boolean isPrivate) throws ExceptionsReader {
+        if (message.isEmpty()) {
+            throw new ExceptionsReader("You can't post an empty tweet !");
+        } else if (message.length() > 280) {
+            throw new ExceptionsReader("Your tweet must not exceed 280 characters !");
+        }
+        m_message = message;
+        m_isPrivate = isPrivate;
+    }
+
+    public void show() {
+        System.out.printf("Username\n@Username\n%s\n%s\n", m_message, m_creationDate);
+    }
+
 
 }
